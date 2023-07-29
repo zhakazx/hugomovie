7be1d7882b525fc6247a2410224f292d2f76b484
zhakazx <zhakazx@gmail.com>
2023-07-29 17:11:02 +0800

    chore: daisyui as theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,14 +5,24 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#3376a5",
+          "secondary": "#60dba8",                        
+          "accent": "#9d6df2",
+          "neutral": "#ffffff",                        
+          "neutral": "#2a3137", 
+          "base-100": "#eef0f6",
+          "info": "#85b2e5",
+          "success": "#5aeda1",
+          "warning": "#fdd55e",
+          "error": "#f65151",
+        },
       },
-    },
+      "light",
+    ],
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 }
