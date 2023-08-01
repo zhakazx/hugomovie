7cec330a68ebf177d1600237396e763f1b5dcc52
zhakazx <zhakazx@gmail.com>
2023-08-01 16:59:27 +0800

    feat(home)(navbar): fix padding

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const index = () => {
   return (
     <div className="p-4">
-        <div className="navbar bg-white shadow-md p-3 rounded-xl">
+        <div className="navbar bg-white shadow-md p-2 md:p-3 rounded-xl">
             <div className="flex-1">
                 <a className="btn btn-ghost text-xl md:text-2xl tracking-wide">HugoMovie</a>
             </div>
